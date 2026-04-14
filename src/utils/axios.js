ee27d31ffd1253b5334f637ece29a3531a0a6893
nesrine <nesrineghenaiar@gmail.com>
2026-04-14 01:39:00 +0100

    Connect frontend to backend API

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create custom axios instance
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: import.meta.env.VITE_API_URL || "https://casnos-backend.onrender.com"
 });
 
 // Interceptor - runs before EVERY request
